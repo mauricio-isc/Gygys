09fdde73d5680d1c8970e1b82671705644aa3922
mauricio-isc <mauriciosan0315@gmail.com>
2025-10-01 19:53:48 -0600

    se establecio el tipo de membresia

---
+++ managament/src/main/java/com/gym/repository/TipoMembresiaRepository.java
@@ -0,0 +1,19 @@
+package com.gym.repository;
+
+import com.gym.entity.TipoMembresia;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TipoMembresiaRepository extends JpaRepository<TipoMembresia, Long> {
+
+    List<TipoMembresia> findByActivoTrue();
+
+    @Query("SELECT t FROM TipoMembresia t WHERE t.activo = true ORDER BY t.precio ASC")
+    List<TipoMembresia> findActivosOrderByPrecio();
+
+    boolean existsByNombre(String nombre)
+}
